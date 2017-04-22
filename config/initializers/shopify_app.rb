addcb07325d0dde87fe973378127ae87a4227800
Feifan Wang <feifanw@gmail.com>
2017-04-22 11:01:43 -0500

    debug gems and modify permission

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -2,6 +2,6 @@ ShopifyApp.configure do |config|
   config.application_name = 'Mega Tags'
   config.api_key = ENV['SHOPIFY_API_KEY']
   config.secret = ENV['SHOPIFY_SECRET']
-  config.scope = 'read_orders, read_products'
+  config.scope = 'read_products, read_themes, write_themes'
   config.embedded_app = true
 end
